14e6c2299ec55c17ad05096cb3739126b2dd59d6
KYC127 <48679612+KYE2138@users.noreply.github.com>
2021-12-12 20:02:02 +0800

    Update cnn_mnist_struct_pru.py

--- configs/cnn_mnist_struct_pru.py
+++ configs/cnn_mnist_struct_pru.py
@@ -3,8 +3,8 @@ from configs.cnn_mnist_aging import training_config, bound_config, search_config
 
 training_config.pruning = PruningConfig(
     structured=True,
-    start_pruning_at_epoch=3,
-    finish_pruning_by_epoch=18,
+    start_pruning_at_epoch=2,
+    finish_pruning_by_epoch=3,
     min_sparsity=0.05,
     max_sparsity=0.8
 )
