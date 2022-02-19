2419789d1601659db346571281ba130e6c95ebf9
KYC127 <48679612+KYE2138@users.noreply.github.com>
2022-02-19 14:22:10 +0800

    Update test_load_pickle_sc.py

--- test_load_pickle_sc.py
+++ test_load_pickle_sc.py
@@ -4,19 +4,9 @@ import pickle
 with open('artifacts/cnn_speech_commands/example_cnn_speech_commands_struct_pru_agingevosearch_state.pickle', 'rb') as f:
   end_point = pickle.load(f)
 print("------------------------------")
-print("end_point:",end_point)
-print("------------------------------")
 print("len of end_point:",len(end_point))
 print("------------------------------")
-print("type of end_point[0]:",type(end_point[0]))
-print("------------------------------")
-print("end_point[0].point.arch:",end_point[0].point.arch)
-print("------------------------------")
-print("end_point[0].point.arch.to_keras_model((28,28,1),10):",end_point[0].point.arch.to_keras_model((28,28,1),10))
-print("------------------------------")
-model = end_point[0].point.arch.to_keras_model((28,28,1),10)
-model.summary()
-
+  
 import numpy as np
 import tensorflow as tf
 
@@ -26,14 +16,13 @@ from resource_models.models import model_size, peak_memory_usage
 
 output_dir = "tmp/tflite"
 
-cnn_arch = end_point[1999].point.arch
-
 input_shape = (49, 40, 1)
 num_classes = 10
 
 
 def convert_to_tflite(arch, output_file):
-    model = arch.to_keras_model( input_shape, num_classes)
+    model = arch.to_keras_model(input_shape, num_classes)
+    model.summary()
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
@@ -48,5 +37,5 @@ def convert_to_tflite(arch, output_file):
     with open(output_file, "wb") as f:
         f.write(model_bytes)
 for i in range(1, 21):
-  cnn_arch = cnn_arch = end_point[i-1].point.arch
-  convert_to_tflite(cnn_arch, output_file=f"{output_dir}/speech_command_end_point[{i}]_point_arch.tflite")
+  cnn_arch = end_point[i*100-1].point.arch
+  convert_to_tflite(cnn_arch, output_file=f"{output_dir}/speech_command_end_point[{i*100-1}]_point_arch.tflite")
