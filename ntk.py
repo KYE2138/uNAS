298b122cecec1e9ff2df24b1d5af18aff5de4f1c
KYC127 <ibiu1222@gmail.com>
2022-05-18 19:17:09 +0800

    Update ntk.py

--- ntk.py
+++ ntk.py
@@ -22,7 +22,7 @@ import gc
 
 #################### GPU ####################
 import os
-os.environ['CUDA_LAUNCH_BLOCKING'] = 1
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 # GPU mem issue
 #config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(allow_growth=True))
 #sess = tf.compat.v1.Session(config=config)
