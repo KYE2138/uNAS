3f748816a0ebc570e798b5cb6b179c984558fb5d
KYC127 <ibiu1222@gmail.com>
2022-05-20 02:04:16 +0800

    Update cnn_vww.py

--- configs/cnn_vww.py
+++ configs/cnn_vww.py
@@ -6,7 +6,7 @@ from dataset import VisualWakeWords
 from cnn import CnnSearchSpace
 
 training_config = TrainingConfig(
-    dataset=VisualWakeWords("/storage/data/visualwakewords"),
+    dataset=VisualWakeWords("/storage/dataset/visualwakewords"),
     optimizer=lambda: tfa.optimizers.SGDW(learning_rate=0.001, weight_decay=5e-5),
     batch_size=1,
     epochs=1,
@@ -24,5 +24,6 @@ bound_config = BoundConfig(
     error_bound=0.10,
     peak_mem_bound=250000,
     model_size_bound=250000,
-    mac_bound=30000000
+    mac_bound=30000000,
+    ntk=5000
 )
