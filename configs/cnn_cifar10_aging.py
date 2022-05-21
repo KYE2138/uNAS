9af6db518152beb851aac13edc202f49ec94b76c
KYC127 <ibiu1222@gmail.com>
2022-05-21 13:12:47 +0800

    Update cnn_cifar10_aging.py

--- configs/cnn_cifar10_aging.py
+++ configs/cnn_cifar10_aging.py
@@ -20,7 +20,7 @@ search_algorithm = AgingEvoSearch
 training_config = TrainingConfig(
     dataset=CIFAR10(),
     optimizer=lambda: tfa.optimizers.SGDW(learning_rate=0.01, momentum=0.9, weight_decay=1e-5),
-    batch_size=128,
+    batch_size=64,
     epochs=130,
     #epochs=1,
     callbacks=lambda: [LearningRateScheduler(lr_schedule)],
