22496e1abcff4f70aa53bf450ea0b14bc67d654d
KYC127 <ibiu1222@gmail.com>
2022-06-03 02:25:36 +0800

    Update cnn_vww_aging_struct_pru.py

--- configs/cnn_vww_aging_struct_pru.py
+++ configs/cnn_vww_aging_struct_pru.py
@@ -11,7 +11,7 @@ search_algorithm = AgingEvoSearch
 training_config = TrainingConfig(
     dataset=VisualWakeWords("/docker/file/dataset/visualwakewords"),
     optimizer=lambda: tfa.optimizers.SGDW(learning_rate=0.001, weight_decay=5e-5),
-    batch_size=16,
+    batch_size=64,
     epochs=30,
     callbacks=lambda: []
     #callbacks=lambda: [EarlyStopping(patience=15, verbose=1)]
@@ -29,7 +29,9 @@ search_config = AgingEvoConfig(
     search_space=CnnSearchSpace(),
     rounds=2000,
     checkpoint_dir="artifacts/cnn_vww",
-    max_parallel_evaluations = 1
+    max_parallel_evaluations = 1,
+    population_size = 40
+    sample_size = 10
 )
 
 bound_config = BoundConfig(
