6be5488faafdf57f64088982881dd7d544a5c4cd
KYC127 <ibiu1222@gmail.com>
2022-06-07 02:11:56 +0800

    Update metrics_file.py

--- metrics_file.py
+++ metrics_file.py
@@ -50,15 +50,24 @@ class ModelMetricsFile:
         def save_dataset(dataset, batch_size, input_shape, num_classes, num_batch, save_path):
             #################### dataset ####################
             #check loader
+            train_loader_save_path = f'{save_path}/train_loader.pickle'
+            val_loader_save_path = f'{save_path}/val_loader.pickle'
+            if os.path.isfile(train_loader_save_path) and os.path.isfile(val_loader_save_path):
+                print (f"train_loader_save_path is already exist:{train_loader_save_path}")
+                print (f"val_loader_save_path is already exist:{val_loader_save_path}")
+            else:
+                print (f"generate train_loader_save_path :{train_loader_save_path}")
+                print (f"generate val_loader_save_path :{val_loader_save_path}")
+            '''   
             loader_save_path = f'{save_path}/loader.npz'
             if os.path.isfile(loader_save_path):
                 print (f"loader is already exist:{loader_save_path}")
             else:
                 print (f"generate loader :{loader_save_path}")
-                
+            '''                 
                 # from uNAS dataset by tf
                 train = dataset.train_dataset() \
-                    .shuffle(100) \
+                    .shuffle(100000) \
                     .batch(batch_size) \
                     .prefetch(tf.data.experimental.AUTOTUNE)
                 # <PrefetchDataset shapes: ((None, 32, 32, 3), (None, 1)), types: (tf.float64, tf.uint8)>
