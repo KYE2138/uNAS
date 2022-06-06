1a9aba0365cca986818dec88aba2fffa9f499b84
KYC127 <ibiu1222@gmail.com>
2022-06-06 22:27:45 +0800

    gpu mem

--- driver.py
+++ driver.py
@@ -30,27 +30,12 @@ def main():
     tf.random.set_seed(args.seed)
     
     # gpu相關
-    '''
+    
     gpus = tf.config.experimental.list_physical_devices("GPU")
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
-    '''
-    # limit gpu mem to load keras model and transfer
-
-    gpus = tf.config.list_physical_devices('GPU')
-    print (gpus)
-    if gpus:
-        # Restrict TensorFlow to only allocate 4GB of memory on the first GPU
-        try:
-            tf.config.set_logical_device_configuration(
-                gpus[0],
-                [tf.config.LogicalDeviceConfiguration(memory_limit=4096)])
-            logical_gpus = tf.config.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Virtual devices must be set before GPUs have been initialized
-            print(e)
-
+    
+    
 
 
     # 檢查參數
