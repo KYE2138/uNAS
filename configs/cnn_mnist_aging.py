ab9d571539140028257bb1e167f92585a77c7710
KYC127 <ibiu1222@gmail.com>
2022-06-07 15:00:44 +0800

    Update cnn_mnist_aging.py

--- configs/cnn_mnist_aging.py
+++ configs/cnn_mnist_aging.py
@@ -10,7 +10,7 @@ search_algorithm = AgingEvoSearch
 training_config = TrainingConfig(
     dataset=MNIST(),
     epochs=30,
-    batch_size=128,
+    batch_size=64,
     optimizer=lambda: tfa.optimizers.SGDW(learning_rate=0.005, momentum=0.9, weight_decay=4e-5),
     callbacks=lambda: [],
 )
