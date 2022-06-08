f0dc4ac275b09b8b7f5fe7a10a2ca15d2629c1ae
KYC127 <ibiu1222@gmail.com>
2022-06-09 03:38:35 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -346,6 +346,6 @@ if __name__ == '__main__':
          "artifacts/cnn_mnist/pre_ntk_cnn_mnist_struct_pru_agingevosearch_state_ntk_1000.pickle"
         ],
         ["uNAS", "uNAS with ntk 1000 bounds"],
-        x_range=(0, 0.20), y_range=(0, 20000), y_key=3,
+        x_range=(0, 0.20), y_range=(0, 30000), y_key=3,
         title="Model size vs error rate Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/MACs_pareto_MNIST.pdf")
