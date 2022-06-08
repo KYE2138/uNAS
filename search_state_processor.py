47f0e440faf5a0858537b3a7a0e8d48d2f072fcb
KYC127 <ibiu1222@gmail.com>
2022-06-09 03:58:21 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -360,7 +360,7 @@ if __name__ == '__main__':
     ##Cifar10
     multiple_pareto_fronts(
         ["artifacts/cnn_cifar10/example_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
-         "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_ntk_4000_block_10_layer_3.pickle",
+         "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
         ],
         ["uNAS", "uNAS with ntk 4000 bounds"],
         x_range=(0, 0.30), y_range=(0, 150000), y_key=1,
