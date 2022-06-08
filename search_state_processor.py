24983bef0774af3f34bda25f83cd15f004387c86
KYC127 <ibiu1222@gmail.com>
2022-06-09 03:51:29 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -324,6 +324,7 @@ if __name__ == '__main__':
     #     title="Model size vs error rate Pareto fronts for Chars74K",
     #     output_file="pareto_chars74k.pdf")
     #
+    ## MNIST
     multiple_pareto_fronts(
         ["artifacts/cnn_mnist/example_cnn_mnist_struct_pru_agingevosearch_state.pickle",
          "artifacts/cnn_mnist/pre_ntk_cnn_mnist_struct_pru_agingevosearch_state_ntk_1000.pickle",
@@ -332,7 +333,7 @@ if __name__ == '__main__':
         ],
         ["uNAS", "uNAS with ntk 1000 bounds", "uNAS with ntk 200 bounds", "uNAS with ntk less 200 bounds"],
         x_range=(0, 0.10), y_range=(0, 8000), y_key=1,
-        title="Model size vs error rate Pareto fronts for MNIST",
+        title="Peak memory usage vs error rate Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/peak_mem_use_pareto_MNIST.pdf")
     multiple_pareto_fronts(
         ["artifacts/cnn_mnist/example_cnn_mnist_struct_pru_agingevosearch_state.pickle",
@@ -353,5 +354,32 @@ if __name__ == '__main__':
         ],
         ["uNAS", "uNAS with ntk 1000 bounds", "uNAS with ntk 200 bounds", "uNAS with ntk less 200 bounds"],
         x_range=(0, 0.20), y_range=(0, 30000), y_key=3,
-        title="Model size vs error rate Pareto fronts for MNIST",
+        title="MACs vs error rate Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/MACs_pareto_MNIST.pdf")
+    
+    ##Cifar10
+    multiple_pareto_fronts(
+        ["artifacts/cnn_cifar10/example_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
+         "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_ntk_4000_block_10_layer_3.pickle"
+        ],
+        ["uNAS", "uNAS with ntk 4000 bounds"],
+        x_range=(0, 0.30), y_range=(0, 150000), y_key=1,
+        title="Peak memory usage vs error rate Pareto fronts for Cifar10",
+        output_file="artifacts/cnn_mnist/peak_mem_use_pareto_Cifar10.pdf")
+    multiple_pareto_fronts(
+        ["artifacts/cnn_cifar10/example_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
+         "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_ntk_4000_block_10_layer_3.pickle"
+        ],
+        ["uNAS", "uNAS with ntk 4000 bounds"],
+        x_range=(0, 0.30), y_range=(0, 150000), y_key=2,
+        title="Model size vs error rate Pareto fronts for Cifar10",
+        output_file="artifacts/cnn_mnist/model_size_pareto_Cifar10.pdf")
+
+    multiple_pareto_fronts(
+        ["artifacts/cnn_cifar10/example_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
+         "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_ntk_4000_block_10_layer_3.pickle"
+        ],
+        ["uNAS", "uNAS with ntk 4000 bounds"],
+        x_range=(0, 0.30), y_range=(0, 50000000), y_key=3,
+        title="MACs vs error rate Pareto fronts for Cifar10",
+        output_file="artifacts/cnn_mnist/MACs_pareto_Cifar10.pdf")
