0e0734e3fb427c2a90024b8c7aafab68f97816f2
KYC127 <ibiu1222@gmail.com>
2022-06-09 04:10:07 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -363,7 +363,7 @@ if __name__ == '__main__':
          "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
         ],
         ["uNAS", "uNAS with ntk 4000 bounds"],
-        x_range=(0.10, 0.30), y_range=(0, 150000), y_key=1,
+        x_range=(0.12, 0.30), y_range=(0, 150000), y_key=1,
         title="PMU vs error rate Pareto fronts for Cifar10",
         output_file="artifacts/cnn_cifar10/peak_mem_use_pareto_Cifar10.pdf")
 
@@ -372,7 +372,7 @@ if __name__ == '__main__':
          "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
         ],
         ["uNAS", "uNAS with ntk 4000 bounds"],
-        x_range=(0.10, 0.30), y_range=(0, 150000), y_key=2,
+        x_range=(0.12, 0.30), y_range=(0, 150000), y_key=2,
         title="Model size vs error rate Pareto fronts for Cifar10",
         output_file="artifacts/cnn_cifar10/model_size_pareto_Cifar10.pdf")
 
@@ -381,6 +381,6 @@ if __name__ == '__main__':
          "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
         ],
         ["uNAS", "uNAS with ntk 4000 bounds"],
-        x_range=(0.10, 0.30), y_range=(0, 50000000), y_key=3,
+        x_range=(0.12, 0.30), y_range=(0, 50000000), y_key=3,
         title="MACs vs error rate Pareto fronts for Cifar10",
         output_file="artifacts/cnn_cifar10/MACs_pareto_Cifar10.pdf")
