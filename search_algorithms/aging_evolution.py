ffbdbef8845bf241af8cda21b3a21e573bee44bb
KYC127 <ibiu1222@gmail.com>
2022-06-09 17:44:51 +0800

    Update aging_evolution.py

--- search_algorithms/aging_evolution.py
+++ search_algorithms/aging_evolution.py
@@ -57,9 +57,9 @@ class GPUTrainer:
         ntks = ModelMetricsFile(self.trainer).get_metrics(model, num_batch=1)
         ntk = np.mean(ntks).astype('int64')
         # ntk_threshold是bound兩倍
-        ntk_threshold = int(self.bound_config.ntk)*2
+        ntk_threshold = int(self.bound_config.ntk)*3
         #pdb.set_trace()
-        if ntk>ntk_threshold :
+        if ntk<0 or ntk>ntk_threshold :
             print(f'ntks = {ntks}, ntk = {ntk}, change epochs = 1')
             results = self.trainer.train_and_eval(model, sparsity=point.sparsity, epochs=1)
         else:
