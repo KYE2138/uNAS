977d9b5eadefa8edb79dd0902f4303c8afcacb5b
KYC127 <ibiu1222@gmail.com>
2022-06-10 21:06:45 +0800

    Update metrics_ntk_rn.py

--- metrics_ntk_rn.py
+++ metrics_ntk_rn.py
@@ -455,7 +455,7 @@ def get_rn(save_path, input_finish_info):
         inputs = loader[0][0]
         input_size = inputs.shape
         #fix rns is always =batch_size=64
-        input_size=(1000, 3, 3, 1)
+        input_size=(10000, 3, 3, 1)
         print (f'input_size={input_size}')
         
 
