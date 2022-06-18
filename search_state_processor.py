029fd07f3e93184223534bbd9f9a3027a4c832c3
KYC127 <ibiu1222@gmail.com>
2022-06-18 20:40:26 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -333,22 +333,22 @@ if __name__ == '__main__':
         ["artifacts/cnn_mnist/example_cnn_mnist_struct_pru_agingevosearch_state.pickle",
          "artifacts/cnn_mnist/pre_ntk_cnn_mnist_struct_pru_agingevosearch_state_ntk_1000.pickle",
          "artifacts/cnn_mnist/EfficientM_5_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
-        "artifacts/cnn_mnist/M_1_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
+        "artifacts/cnn_mnist/M_2_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
         "artifacts/cnn_mnist/ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle"
         ],
-        ["uNAS", "ntk 1000", "EfficientM_5","M_1","ntk 1000 rn 1500"],
-        x_range=(0, 0.10), y_range=(0, 5000), y_key=1,
+        ["uNAS", "ntk 1000", "EfficientM_5","M_2","ntk 1000 rn 1500"],
+        x_range=(0, 0.10), y_range=(0, 5000), y_key=1, take_n=200,
         title="PMU vs error rate Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/peak_mem_use_pareto_MNIST.pdf")
     multiple_pareto_fronts(
          ["artifacts/cnn_mnist/example_cnn_mnist_struct_pru_agingevosearch_state.pickle",
          "artifacts/cnn_mnist/pre_ntk_cnn_mnist_struct_pru_agingevosearch_state_ntk_1000.pickle",
          "artifacts/cnn_mnist/EfficientM_5_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
-        "artifacts/cnn_mnist/M_1_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
+        "artifacts/cnn_mnist/M_2_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
         "artifacts/cnn_mnist/ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle"
         ],
-        ["uNAS", "ntk 1000", "EfficientM_5","M_1","ntk 1000 rn 1500"],
-        x_range=(0, 0.10), y_range=(0, 5000), y_key=2,
+        ["uNAS", "ntk 1000", "EfficientM_5","M_2","ntk 1000 rn 1500"],
+        x_range=(0, 0.10), y_range=(0, 5000), y_key=2, take_n=200,
         title="Model size vs error rate Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/model_size_pareto_MNIST.pdf")
 
@@ -356,11 +356,11 @@ if __name__ == '__main__':
        ["artifacts/cnn_mnist/example_cnn_mnist_struct_pru_agingevosearch_state.pickle",
          "artifacts/cnn_mnist/pre_ntk_cnn_mnist_struct_pru_agingevosearch_state_ntk_1000.pickle",
          "artifacts/cnn_mnist/EfficientM_5_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
-        "artifacts/cnn_mnist/M_1_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
+        "artifacts/cnn_mnist/M_2_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
         "artifacts/cnn_mnist/ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle"
         ],
-        ["uNAS", "ntk 1000", "EfficientM_5","M_1","ntk 1000 rn 1500"],
-        x_range=(0, 0.10), y_range=(0, 50000), y_key=3,
+        ["uNAS", "ntk 1000", "EfficientM_5","M_2","ntk 1000 rn 1500"],
+        x_range=(0, 0.10), y_range=(0, 50000), y_key=3, take_n=200,
         title="MACs vs error rate Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/MACs_pareto_MNIST.pdf")
     
