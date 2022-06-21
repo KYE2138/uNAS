a581d7b0dea48fa1fc35d7e70a6cd79972b2109c
KYC127 <ibiu1222@gmail.com>
2022-06-21 22:23:44 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -385,7 +385,7 @@ if __name__ == '__main__':
          "artifacts/cnn_cifar10/EfficientM_7_ntk_4000_rn_1500_cnn_cifar10_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
          "artifacts/cnn_cifar10/M_3_ntk_4000_rn_1500_cnn_cifar10_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle"
         ],
-        ["uNAS", "uNAS with ntk 4000 bounds","EfficientM_5","EfficientM_7","M_2","M_3"],
+        ["uNAS", "uNAS with ntk 4000 bounds","EfficientM_7","M_2","M_3"],
         x_range=(0.12, 0.30), y_range=(0, 150000), y_key=1, take_n=200,
         title="PMU vs error rate Pareto fronts for Cifar10",
         output_file="artifacts/cnn_cifar10/peak_mem_use_pareto_Cifar10.pdf")
@@ -397,7 +397,7 @@ if __name__ == '__main__':
          "artifacts/cnn_cifar10/EfficientM_7_ntk_4000_rn_1500_cnn_cifar10_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
          "artifacts/cnn_cifar10/M_3_ntk_4000_rn_1500_cnn_cifar10_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle"
         ],
-        ["uNAS", "uNAS with ntk 4000 bounds","EfficientM_5","EfficientM_7","M_2","M_3"],
+        ["uNAS", "uNAS with ntk 4000 bounds","EfficientM_7","M_2","M_3"],
         x_range=(0.12, 0.30), y_range=(0, 150000), y_key=2, take_n=200,
         title="Model size vs error rate Pareto fronts for Cifar10",
         output_file="artifacts/cnn_cifar10/model_size_pareto_Cifar10.pdf")
