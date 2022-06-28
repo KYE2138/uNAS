9ef406cc6f932775303e6797b5d02eb1f6ef9da6
KYC127 <ibiu1222@gmail.com>
2022-06-28 13:35:34 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -371,7 +371,7 @@ if __name__ == '__main__':
          "artifacts/cnn_cifar10/oM_1_ntk_4000_rn_1500_cnn_cifar10_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle"
          ],
         ["uNAS", "uNAS with ntk 4000 bounds", "M_6", "oM_1"],
-        x_range=(0.11, 0.30), y_range=(0, 50000), y_key=1, take_n=480,
+        x_range=(0.11, 0.30), y_range=(0, 200000), y_key=1, take_n=480,
         title="PMU vs error rate Pareto fronts for Cifar10",
         output_file="artifacts/cnn_cifar10/peak_mem_use_pareto_Cifar10.pdf")
 
