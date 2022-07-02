e7a1c766223975cb291dd06551352f57ea7fd83c
KYC127 <ibiu1222@gmail.com>
2022-07-02 17:36:53 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -598,9 +598,10 @@ if __name__ == '__main__':
     multi_steps_multi_metrics_multiple_pareto_fronts(
          ["artifacts/cnn_cifar10/example_cnn_cifar10_struct_pru_2_agingevosearch_state.pickle",
          "artifacts/cnn_cifar10/pre_ntk_cnn_cifar10_struct_pru_agingevosearch_state.pickle",
+         "artifacts/cnn_cifar10/EfficientM_6_ntk_1000_rn_1500_cnn_mnist_struct_pru_ntk_rn_block_10_layer_3_agingevosearch_state.pickle",
          ],
-        ["uNAS", "Ntk"],
-        x_range=(0.11, 0.40), y_range=(0, 10000000), y_key=[1,2,3], take_n=320, every_n=80,
+        ["uNAS", "Ntk","EfficientM_6"],
+        x_range=(0.11, 0.40), y_range=(0, 10000000), y_key=[1,2,3], take_n=800, every_n=400,
         title="Pareto fronts for Cifar10",
         output_file="artifacts/cnn_cifar10/test_multi_steps_multi_metrics_pareto_Cifar10_PMU_MS_MACs.png")
     
