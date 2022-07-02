a996e646461df4f7a8991b89f1016f84f7c1ac92
KYC127 <ibiu1222@gmail.com>
2022-07-03 03:13:53 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -549,8 +549,9 @@ if __name__ == '__main__':
     multi_steps_multi_metrics_multiple_pareto_fronts(
         ["artifacts/cnn_mnist/example_cnn_mnist_struct_pru_b64_agingevosearch_state.pickle",
          "artifacts/cnn_mnist/mnist_ntk_2_pre_search_agingevosearch_state.pickle",
+         "artifacts/cnn_mnist/pre_ntk_cnn_mnist_struct_pru_agingevosearch_state_ntk_1000_block_10_layer_3.pickle",
         ],
-        ["uNAS","ntk_2_pre_search",],
+        ["uNAS_b64","ntk_2_pre_search","pre_ntk"],
         x_range=(0, 0.10), y_range=(0, 400000), y_key=[1,2,3], take_n=1000, every_n=500, first_pop=False,
         title="Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/test_multi_steps_multi_metrics_pareto_MNIST_PMU_MS_MACs.png")
