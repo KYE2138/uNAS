811deb88e93162583fae9dacebee44b362521f78
KYC127 <ibiu1222@gmail.com>
2022-07-04 00:51:26 +0800

    Update search_state_processor.py

--- search_state_processor.py
+++ search_state_processor.py
@@ -557,7 +557,7 @@ if __name__ == '__main__':
          #"ntk_2_pre_search",
          "pre_ntk"
          ],
-        x_range=(0, 0.06), y_range=(0, 200000), y_key=[1,2,3], take_n=1000, every_n=250, first_pop=False,
+        x_range=(0, 0.06), y_range=(0, 50000), y_key=[1,2,3], take_n=1000, every_n=250, first_pop=False,
         title="Pareto fronts for MNIST",
         output_file="artifacts/cnn_mnist/test_multi_steps_multi_metrics_pareto_MNIST_PMU_MS_MACs.png")
     
